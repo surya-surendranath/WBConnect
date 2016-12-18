070671957d50d469c416afc86106e18a10ec9cdd
surya-surendranath <surya.snath08@gmail.com>
2016-12-19 00:39:16 +1300

    committ

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var users = require('./routes/users');
 
 var app = express();
 
-app.set('port', (process.env.PORT || 5000));
+app.set('port', (process.env.PORT || 3000));
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
